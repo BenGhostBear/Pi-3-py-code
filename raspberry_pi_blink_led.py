989e31a2de27dfb2127d9e869a5aaee46a52a200
Damian <147666866+BenGhostBear@users.noreply.github.com>
2024-05-06 13:48:32 -0700

    Create raspberry_pi_blink_led.py

---
+++ raspberry_pi_blink_led.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+
+blink_led = 5
+GPIO.setmode(GPIO.BOARD) # Note This is specifying the physical pins on the Raspberry Pi Header 
+GPIO.setwarnings(False)
+GPIO.setup(blink_led,GPIO.OUT)
+
+while(True):
+  print("LED on")
+  GPIO.output(blink_led,GPIO.HIGH)
+  time.sleep(1)
+  print("LED off")
+  GPIO.output(blink_led,GPIO.LOW)
+  time.sleep(1)
+print('Done')
